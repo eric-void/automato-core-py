2ab1ea2088c904ddfa995f8376fd60f362f733f0
Eric <eric@void.it>
2022-01-25 22:36:06 +0100

    Scripting js: removed script_eval_cache and obsolete code

--- src/automato/core/scripting_js.py
+++ src/automato/core/scripting_js.py
@@ -1,6 +1,9 @@
 # require python3
 # -*- coding: utf-8 -*-
 
+# NOTE script_eval_cache DISABLED because it was not working property:
+# - not working properly with some payload values - probably float ones - the same event was emitted with different payloads
+# - script_js_compiled optimizations works much better, and it seems there is no need for a cache layer right now
 # TODO Impostare anche gli "exports"
 
 import logging
@@ -19,6 +22,9 @@ from automato.core import utils
 # @see https://github.com/PiotrDabkowski/Js2Py/blob/master/js2py/evaljs.py - https://github.com/PiotrDabkowski/Js2Py/blob/master/js2py/translators/translator.py
 js2py_use_compilation_plan = False
 
+"""
+NOTE script_eval_cache DISABLED (not working properly, and new optimization to script evaluating are much better
+
 script_eval_cache = {}
 script_eval_cache_lock = threading.Lock()
 script_eval_cache_hits = 0
@@ -28,6 +34,7 @@ script_eval_cache_skipped = 0
 script_eval_codecontext_signatures = {}
 SCRIPT_EVAL_CACHE_MAXSIZE = 1024
 SCRIPT_EVAL_CACHE_PURGETIME = 3600
+"""
 script_context_instance = None
 script_context_instance_context_keys = None
 script_context_instance_exports_keys = None
@@ -38,8 +45,6 @@ script_js_compiled_hits = 0
 script_js_compiled_miss = 0
 SCRIPT_JS_COMPILED_MAXSIZE = 1000
 SCRIPT_JS_COMPILED_PURGETIME = 3600
-
-#script_eval_quick_count = 0
 
 exports = {}
 
@@ -91,15 +96,11 @@ def script_context(context = {}):
   return script_context_instance
 
 def script_eval(code, context = {}, to_dict = False, cache = False):
-  #global script_eval_quick_count
+  """
+  @param cache Code excecution can be cached (this parameter is NOT used right now)
+  """
+  
   _s = system._stats_start()
-  """
-  ret = _script_eval_quick(code, context)
-  if ret and 'return' in ret:
-    script_eval_quick_count = script_eval_quick_count + 1
-    ret = ret['return']
-  else:
-  """
   ret = _script_eval_int(code, context, cache)
   if ret and to_dict and isinstance(ret, js2py.base.JsObjectWrapper):
     ret = ret.to_dict()
@@ -108,14 +109,14 @@ def script_eval(code, context = {}, to_dict = False, cache = False):
   return ret
 
 def _script_eval_int(code, context = {}, cache = False):
+  """
+  NOTE script_eval_cache DISABLED (not working properly, and new optimization to script evaluating are much better
+  
   global script_eval_cache, script_eval_cache_lock, script_eval_cache_hits, script_eval_cache_miss, script_eval_cache_skipped, script_eval_cache_disabled, script_eval_codecontext_signatures
   
-  cache = False
-
   # TODO UNSUPPORTED: If context is the result of another exec/eval call, i must extract the real context from it. I'm skipping it right now
   # NOTE FUTURE: context._var._obj.own è un oggetto di tipo Scope (vedi https://github.com/PiotrDabkowski/Js2Py/blob/master/js2py/base.py#L1066) che contiene TUTTO l'ambiente js (quindi le variabili di context, ma anche tutto il resto)
   # Es: context._var._obj.own.keys() mi da tutti i nomi. Da li posso estrarre il context aggiornato
-
   if cache:
     if len(script_eval_cache) > SCRIPT_EVAL_CACHE_MAXSIZE:
       with script_eval_cache_lock:
@@ -136,33 +137,11 @@ def _script_eval_int(code, context = {}, cache = False):
     if cache:
       context_sorted = sorted(context)
       
-      """
-      contextkey = {}
-      for v in context_sorted:
-        if isinstance(context[v], dict):
-          contextkey[v] = {}
-          for vv in context[v]:
-            contextkey[v][vv] = ''
-        else:
-          contextkey[v] = ''
-      """
       # CONTEXT: part contains the first and second-level keys of context object, in the form { key: '', key: { dictkey: ''}}
       codecontext_signature = "CODE:" + code + ",CONTEXT:" + str({x: ('' if not isinstance(context[x], dict) else {y: '' for y in sorted(context[x])}) for x in context_sorted})
       
       with script_eval_cache_lock:
         if not codecontext_signature in script_eval_codecontext_signatures:
-          """
-          script_eval_codecontext_signatures[codecontext_signature] = {}
-          for v in context_sorted:
-            if re.search(r'\b' + v + r'\b', code):
-              if isinstance(context[v], dict):
-                script_eval_codecontext_signatures[codecontext_signature][v] = {}
-                for vv in context[v]:
-                  if re.search(r'\b' + vv + r'\b', code):
-                    script_eval_codecontext_signatures[codecontext_signature][v][vv] = ''
-              else:
-                script_eval_codecontext_signatures[codecontext_signature][v] = ''
-          """
           # This struct contains the usage of context keys (first and second level) in the code. If a key is present, with value '', that key is used in the code as is. If not present, it's not used. If it's a dict, it reflects the usage of subkeys.
           script_eval_codecontext_signatures[codecontext_signature] = { x: ('' if not isinstance(context[x], dict) or _script_code_uses_full_var(code, x) else {y: '' for y in sorted(context[x]) if re.search(r'\b' + y + r'\b', code) }) for x in context_sorted if re.search(r'\b' + x + r'\b', code) }
 
@@ -183,6 +162,7 @@ def _script_eval_int(code, context = {}, cache = False):
       script_eval_cache_skipped += 1
   else:
     script_eval_cache_disabled += 1
+  """
   
   # TODO Supporto per altri linguaggi
   if code.startswith('js:'):
@@ -192,21 +172,26 @@ def _script_eval_int(code, context = {}, cache = False):
   else:
     ret = _script_exec_js(code, context, do_eval = True)
     
+  """
+  NOTE script_eval_cache DISABLED (not working properly, and new optimization to script evaluating are much better
+  
   if cache and not ret['error']:
     with script_eval_cache_lock:
       script_eval_cache[keyhash] = { 'key': key, 'used': system.time(), 'result': ret['return'] }
+  """
   return ret['return'] if 'return' in ret else None
   
+"""
+NOTE script_eval_cache DISABLED (not working properly, and new optimization to script evaluating are much better
+
 def _script_code_uses_full_var(code, var):
-  """
-  Return if code uses the var, without dict key reference ("payload[x]" or "x in payload" uses key reference, "payload" not)
-  """
-  #return re.search(r'\b' + var + r'(\.|\[)', code)
+  # Return if code uses the var, without dict key reference ("payload[x]" or "x in payload" uses key reference, "payload" not)
   parts = re.split(r'\b' + var + r'\b', code)
   for i in range(0, len(parts) - 1):
     if not re.search(r'\b(typeof|in)\s*$', parts[i]) and not re.search(r'^(\.|\[)', parts[i + 1]):
       return True
   return False
+"""
 
 def script_exec(code, context = {}, return_context = True):
   """
@@ -336,22 +321,3 @@ def _payload_transfer(base_result, payload, keys, empty_result = None):
       if k in payload:
         result[keys[k] if keys[k] else k] = payload[k]
   return {** base_result, ** result} if result else empty_result
-
-"""
-def _script_eval_quick(code, context):
-  if code == '({value: payload})':
-    return {'return': {'value': context['payload']}}
-  for k in ['hp', 'day_plugs1', 'night_plugs', 'basement_light', 'basement_plugs', 'external', 'irrigation_pump', 'basement_pump', 'hp_heating', 'hp_dhw', 'other']:
-    if code == "js:('" + k + "' in payload ? {port: '" + k + "', energy: parseFloat(payload['" + k + "']['energy_result']), power: parseInt(payload['" + k + "']['power_last'])} : null)":
-      return {'return': {'port': k, 'energy': _parse_float(context['payload'][k]['energy_result']), 'power': _parse_int(context['payload'][k]['power_last'])} if k in context['payload'] else None }
-  if code == 'js:({power: parseFloat(payload), port: matches[1] ? matches[2] : "0"})':
-    return {'return': { 'power': _parse_float(context['payload']), 'port': context["matches"][2] if context["matches"][1] else "0" }}
-  if code == 'js:({energy: parseFloat(payload) / 60000, energy_reported: parseFloat(payload), port: matches[1] ? matches[2] : "0"})':
-    return {'return': { 'energy': _parse_float(context['payload']) / 60000, 'energy_reported': _parse_float(context['payload']), 'port': context["matches"][2] if context["matches"][1] else "0" }}
-  if code == 'js:({energy: parseFloat(payload) / 1000, energy_reported: parseFloat(payload), port: matches[1] ? matches[2] : "0"})':
-    return {'return': { 'energy': _parse_float(context['payload']) / 1000, 'energy_reported': _parse_float(context['payload']), 'port': context["matches"][2] if context["matches"][1] else "0" }}
-  if code == 'js:({energy_returned: parseFloat(payload) / 1000, energy_returned_reported: parseFloat(payload), port: matches[1] ? matches[2] : "0"})':
-    return {'return': { 'energy_returned': _parse_float(context['payload']) / 1000, 'energy_returned_reported': _parse_float(context['payload']), 'port': context["matches"][2] if context["matches"][1] else "0" }}
-  if code == 'js:payload == "on" ? ({value: 1, port: matches[1]}) : (payload == "off" || payload == "overpower" ? ({value: 0, port: matches[1]}) : false)':
-    return {'return': {'value': 1, 'port': context['matches'][1]} if context['payload'] == 'on' else ({'value': 0, 'port': context['matches'][1]} if context['payload'] == 'off' or context['payload'] == 'overpower' else False)}
-"""
